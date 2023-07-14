edc6acea6ee2219e17b4751cfc52746f1afc32d2
github-action-benchmark <github@users.noreply.github.com>
2023-07-14 07:25:43 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 950f783e94d6eb27c19f5e400f5139b217077932

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1689319543873,
+  "repoUrl": "https://github.com/96malhar/aws-dotnet-messaging",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "96malhar@gmail.com",
+            "name": "Malhar Khimsaria",
+            "username": "96malhar"
+          },
+          "committer": {
+            "email": "96malhar@gmail.com",
+            "name": "Malhar Khimsaria",
+            "username": "96malhar"
+          },
+          "distinct": true,
+          "id": "950f783e94d6eb27c19f5e400f5139b217077932",
+          "message": "Add benchamrks",
+          "timestamp": "2023-07-14T00:24:18-07:00",
+          "tree_id": "5cebc6e546e561ecd3c035e885fc6afc5fadc8c3",
+          "url": "https://github.com/96malhar/aws-dotnet-messaging/commit/950f783e94d6eb27c19f5e400f5139b217077932"
+        },
+        "date": 1689319543462,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "AWS.Messaging.Benchmark.MessagingBenchmark.PublishMessage",
+            "value": 12813.840843200684,
+            "unit": "ns",
+            "range": "± 35.18340689138598"
+          }
+        ]
+      }
+    ]
+  }
+}
